c4dfa6d1de938efb925be41c93178e6b50b09e12
Viviank <vivianne35kerubo@gmail.com>
2021-07-14 21:18:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import utils
+from utils import load_df
 
+for c in collections:
+    download(c)
+
+train_df = load_df('ref_south_africa_crops_competition_v1_train_labels')
+test_df = load_df('ref_south_africa_crops_competition_v1_test_labels')
