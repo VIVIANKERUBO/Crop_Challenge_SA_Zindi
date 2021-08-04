00d6d5094058de566f97b1d3aba6e14578231421
VIVIANKERUBO <vivianne35kerubo@gmail.com>
2021-08-04 15:48:10 +0200

    main set

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import numpy as np
 import torch
+import pandas as pd
+import utils
+from utils import metrics
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-def read_data(input_csv):
 
+def read_data(input_csv):
+  s1 = pd.read_csv(input_csv)
+  s1 = s1.to_numpy(s1)
   # assign X and y
-  X = 
-  y =
+  X = s1[:, 1:20] 
+  y = s1[:, -2]
+  
 
   # normalize 
   scaler = StandardScaler()
@@ -15,7 +21,7 @@ def read_data(input_csv):
   #X = scaler.transform(X)) use when making predictions
 
   #split to train and test
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42, stratify=y)
 
   #convert to torch
   X_train =  torch.from_numpy(X_train)
@@ -33,6 +39,67 @@ def read_data(input_csv):
   test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64)
 
   return train_loader, test_loader
+
+def train_epoch(model, optimizer, criterion, dataloader, device):
+   model.train()
+   losses = list()
+   with tqdm(enumerate(dataloader), total=len(dataloader), leave=True) as iterator:
+     for idx, batch in iterator:
+       optimizer.zero_grad()
+       x, y = batch
+       x = x.float()
+       x, y = x.to(device), y.to(device)
+       output = model.forward(x)
+       loss =  criterion(output, torch.max(y,1)[1])
+       loss.backward()
+       optimizer.step()
+       losses.append(loss)
+   return torch.stack(losses)
+
+def test_epoch(model,criterion, dataloader,device):
+  model.eval()
+  with torch.no_grad():
+    losses = list()
+    y_true_list = list()
+    y_pred_list = list()
+    with tqdm(enumerate(dataloader), total=len(dataloader), leave=True) as iterator:
+      for idx, batch in iterator:
+        x_test, y_true = batch
+        x_test = x_test.float()
+        y_pred = model.forward(x_test.to(device))
+        loss = criterion(y_pred, y_true.to(device))
+        losses.append(loss)
+        y_true_list.append(y_true)
+        y_pred_list.append(y_pred.argmax(-1))
+  return torch.stack(losses), torch.cat(y_true_list), torch.cat(y_pred_list)
+
+def training(model, epochs, trainloader, testloader):
+
+  criterion = torch.nn.CrossEntropyLoss(reduction="mean")
+  optimizer = Adam(model.parameters(), lr=0.001)
+  scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=50, min_lr=0.0001)
+
+  for epoch in range(epochs):
+    
+    train_loss = train_epoch(model, optimizer, criterion, trainloader, device)
+    train_loss = train_loss.cpu().detach().numpy()[0]
+    test_loss, y_true, y_pred  = test_epoch(model, optimizer, criterion, testloader, device)
+    test_loss = test_loss.cpu().detach().numpy()[0]
+    
+    scores = metrics(y_true_.cpu(), y_pred.cpu())
+    scores_msg = ", ".join([f"{k}={v:.2f}" for (k, v) in scores.items()])
+
+    print(f"epoch {epoch}: trainloss {train_loss:.2f}, testloss {test_loss:.2f} " + scores_msg)
+    scores["epoch"] = epoch
+    scores["trainloss"] = train_loss
+    scores["testloss"] = test_loss
+    log.append(scores)
+
+    log_df = pd.DataFrame(log).set_index("epoch")
+    log_df.to_csv(os.path.join(logdir, "trainlog.csv"))
+    
+    
+  
 
   
 
--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import json
 from pathlib import Path
 import pandas as pd
 
+
 os.environ['MLHUB_API_KEY'] = 'b8d7b4412bc94350c6716ab291a3bb64d39b159794347e4244edcceed1dd739b'
 
 collections = [
@@ -81,5 +82,29 @@ from utils import load_df
 
 train_df = load_df('ref_south_africa_crops_competition_v1_train_labels')
 test_df = load_df('ref_south_africa_crops_competition_v1_test_labels')
+def metrics(y_true, y_pred):
+    accuracy = sklearn.metrics.accuracy_score(y_true, y_pred)
+    kappa = sklearn.metrics.cohen_kappa_score(y_true, y_pred)
+    f1_micro = sklearn.metrics.f1_score(y_true, y_pred, average="micro")
+    f1_macro = sklearn.metrics.f1_score(y_true, y_pred, average="macro")
+    f1_weighted = sklearn.metrics.f1_score(y_true, y_pred, average="weighted")
+    recall_micro = sklearn.metrics.recall_score(y_true, y_pred, average="micro")
+    recall_macro = sklearn.metrics.recall_score(y_true, y_pred, average="macro")
+    recall_weighted = sklearn.metrics.recall_score(y_true, y_pred, average="weighted")
+    precision_micro = sklearn.metrics.precision_score(y_true, y_pred, average="micro")
+    precision_macro = sklearn.metrics.precision_score(y_true, y_pred, average="macro")
+    precision_weighted = sklearn.metrics.precision_score(y_true, y_pred, average="weighted")
 
+    return dict( accuracy=accuracy,
+                 kappa=kappa,
+                 f1_micro=f1_micro,
+                 f1_macro=f1_macro,
+                 f1_weighted=f1_weighted,
+                 recall_micro=recall_micro,
+                 recall_macro=recall_macro,
+                 recall_weighted=recall_weighted,
+                 precision_micro=precision_micro,
+                 precision_macro=precision_macro,
+                 precision_weighted=precision_weighted )
+                                             
 
